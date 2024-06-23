893b79068fe987f249332b67cf399d0bebe5a65b
Kostiantyn Doichev <doichev.kostia@gmail.com>
2024-06-23 14:55:38 +0200

    use async generator

--- iterator.test.ts
+++ iterator.test.ts
@@ -4,7 +4,7 @@ import { afterAll, beforeAll, test } from "bun:test";
 import { setupServer } from "msw/node";
 import { toIterable } from "./iterator.ts";
 
-const address = new URL("http://localhost:3000")
+const address = new URL("http://localhost:3000");
 
 function getQueryParams(url: string) {
 	const queryString = url.split("?")[1];
@@ -26,12 +26,12 @@ const handlers = [
 		const page = Number(pageParam);
 		const perPage = Number(perPageParam);
 
-		assert.ok(page > 0)
+		assert.ok(page > 0);
 
 		const items = Array.from<number>({length: perPage});
 		const start = perPage * (page - 1);
 
-		for (let i = 0; i < perPage ; i += 1) {
+		for (let i = 0; i < perPage; i += 1) {
 			// classic off-by one
 			items[i] = start + i + 1;
 		}
@@ -77,6 +77,26 @@ async function listItems(msg: ListItemsMessage): Promise<{ items: number[], coun
 	return data;
 }
 
+async function listItemsSmarter(msg: ListItemsMessage): Promise<[{
+	items: number[],
+	count: number
+}, null] | [null, Error]> {
+	const params = new URLSearchParams();
+	params.set("total", msg.total.toString());
+	params.set("page", msg.page.toString());
+	params.set("per_page", msg.perPage.toString());
+
+	const response = await fetch(new URL(`/items?${params}`, address));
+
+	if (!response.ok) {
+		const msg = await response.text();
+		return [null, new Error(msg)];
+	}
+
+	const data = await response.json();
+	return [data, null];
+}
+
 test("iterable", async () => {
 	const total = 20;
 	const perPage = 5;
@@ -101,7 +121,52 @@ test("iterable", async () => {
 
 	assert.strictEqual(allItems.length, total);
 	assert.strictEqual(allItems[0], 1);
-	assert.strictEqual(allItems.at(-1), total)
+	assert.strictEqual(allItems.at(-1), total);
 });
 
+test("request throws", async () => {
+	const total = 20;
+	const perPage = 5;
 
+	const iterable = toIterable(({page}) => listItemsSmarter({total, perPage, page}), {
+		initialPage: 0, // invalid parameter
+		getNextPage: (lastResponse, page) => {
+			const [response, error] = lastResponse;
+			if (error) {
+				return null;
+			}
+			const nextPage = page + 1;
+			if (nextPage * perPage <= response.count) {
+				return nextPage;
+			} else {
+				return null;
+			}
+		}
+	});
+
+	let iter = 0;
+	for await (const [response, error] of iterable) {
+		iter += 1;
+		assert.ok(error);
+	}
+
+	assert.strictEqual(iter, 1);
+});
+
+test("panic", async () => {
+	const iterable = toIterable(() => fetch("/non-existing"), {
+		initialPage: 1,
+		getNextPage() {
+			return 1;
+		}
+	});
+
+	try {
+		for await (const res of iterable) {
+			assert.fail("Should've panicked");
+		}
+	} catch (error) {
+		assert.ok(error instanceof Error);
+		assert.notStrictEqual(error.message, "Should've panicked")
+	}
+});
--- iterator.ts
+++ iterator.ts
@@ -10,30 +10,18 @@ export type PaginationOptions<Page> = {
 }
 export type IterableFunction<Page> = (pagination: PaginationOptions<Page>) => Promise<any>
 
-export function toIterable<Page, Func extends IterableFunction<Page>, Res = Awaited<ReturnType<Func>>>(func: Func, options: IterableOptions<Page, Res>): AsyncIterable<Res> {
-	return {
-		[Symbol.asyncIterator]() {
-			let page = options.initialPage;
-			// when done: true, the value is not used
-			let leapReturn = false;
+export async function* toIterable<Page, Func extends IterableFunction<Page>, Res = Awaited<ReturnType<Func>>>(func: Func, options: IterableOptions<Page, Res>): AsyncGenerator<Res> {
+	let page = options.initialPage;
+	while (true) {
+		const response = await func({page});
+		const nextPage = options.getNextPage(response, page);
 
-			return {
-				async next(): Promise<IteratorResult<Res>> {
-					if (leapReturn) {
-						return {done: true, value: undefined};
-					}
+		yield response;
 
-					const response: Res = await func({page});
-					const nextPage = options.getNextPage(response, page);
-					if (nextPage == null) {
-						leapReturn = true;
-					} else {
-						page = nextPage;
-					}
-					return {done: false, value: response};
-				},
-			};
+		if (nextPage == null) {
+			break;
+		} else {
+			page = nextPage;
 		}
-
-	};
+	}
 }
